a539fb00302c1561e6259ef6bd992c475768374a
Botho Hohbaum <hohbaum@adodev.de>
2012-07-09 17:12:17 +0200

    bugfix in smtp class

--- include/libcompactmvc/smtp.php
+++ include/libcompactmvc/smtp.php
@@ -27,51 +27,62 @@ class SMTP {
 	}
 	
 	public function send() {
+		if (function_exists("gethostname")) {
+			$hostname = gethostname();
+		} elseif (function_exists("exec")) {
+			$hostname = exec('hostname -f');
+		} else {
+			// this will probably make problems
+			$hostname = "localhost";
+		}
 		$mailarr = explode("\n", $this->mail);
+		foreach ($mailarr as $key => $val) {
+			$mailarr[$key] = str_replace("\r", "", $val);
+		}
 		$sock = new Socket($this->server, 25);
 		$ret = $sock->read();
-		$sock->write("HELO ".gethostname()."\n");
+		$sock->write("HELO ".$hostname."\r\n");
 		$ret = $sock->read();
 		if (($this->user != "") && ($this->pass != "")) {
-			$sock->write("AUTH LOGIN\n");
+			$sock->write("AUTH LOGIN\r\n");
 			$ret = $sock->read();
-			$sock->write(base64_encode($this->user)."\n");
+			$sock->write(base64_encode($this->user)."\r\n");
 			$ret = $sock->read();
-			$sock->write(base64_encode($this->pass)."\n");
+			$sock->write(base64_encode($this->pass)."\r\n");
 			$ret = $sock->read();
 			if (strpos(strtolower($ret), "535") !== false) {
-				$sock->write("QUIT\n");
+				$sock->write("QUIT\r\n");
 				$sock->read();
 				throw new Exception("SMTP authentication failed: ".$ret, 535);
 			}
 		}
-		$sock->write("MAIL FROM:".$this->sender."\n");
+		$sock->write("MAIL FROM:".$this->sender."\r\n");
 		$ret = $sock->read();
 		if (strpos(strtolower($ret), "250") === false) {
-			$sock->write("QUIT\n");
+			$sock->write("QUIT\r\n");
 			$sock->read();
 			throw new Exception("Could not set sender: ".$ret, substr($ret, 0, 3));
 		}
-		$sock->write("RCPT TO:".$this->receiver."\n");
+		$sock->write("RCPT TO:".$this->receiver."\r\n");
 		$ret = $sock->read();
 		if (strpos(strtolower($ret), "250") === false) {
-			$sock->write("QUIT\n");
+			$sock->write("QUIT\r\n");
 			$sock->read();
 			throw new Exception("Could not set receipient: ".$ret, substr($ret, 0, 3));
 		}
-		$sock->write("DATA\n");
+		$sock->write("DATA\r\n");
 		$ret = $sock->read();
 		foreach ($mailarr as $m) {
-			$sock->write($m."\n");
+			$sock->write($m."\r\n");
 		}
-		$sock->write(".\n");
+		$sock->write(".\r\n");
 		$ret = $sock->read();
 		if (strpos(strtolower($ret), "250") === false) {
-			$sock->write("QUIT\n");
+			$sock->write("QUIT\r\n");
 			$sock->read();
 			throw new Exception("Error during mail transmission: ".$ret, substr($ret, 0, 3));
 		}
-		$sock->write("QUIT\n");
+		$sock->write("QUIT\r\n");
 		$ret = $sock->read();
 		if (strpos(strtolower($ret), "221") === false) {
 			throw new Exception("Notice: Could not close connection cleanly: ".$ret, substr($ret, 0, 3));
