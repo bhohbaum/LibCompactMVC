0819b34f348be31a43299f69e25460cdc0e13603
Botho Hohbaum <hohbaum@adrodev.de>
2014-04-03 12:06:53 +0200

    update

--- application/dba/dba.php
+++ application/dba/dba.php
@@ -4,9 +4,9 @@
  * Database functions
  *
  * @author		Botho Hohbaum (bhohbaum@googlemail.com)
- * @package	LibCompactMVC
+ * @package		LibCompactMVC
  * @copyright	Copyright (c) Botho Hohbaum 24.01.2012
- * @license	LGPL version 3
+ * @license		LGPL version 3
  * @link		https://github.com/bhohbaum/libcompactmvc
  */
 class DBA extends DbAccess {
